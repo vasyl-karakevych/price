7e2e3ddfb20d9c70579b765c0982828bbdf8c175
Vasyl Karakevych <vasyl@karakevych@gmail.com>
2022-07-21 09:36:27 +0300

    marpa - correct price

--- marpa.py
+++ marpa.py
@@ -11,17 +11,27 @@ def exists(path):
     print(f"File: {path} is exists")
     return True
 
+#convert fild PRICE in normal number
+def PriceToBrutto(price):
+    price = price.split(' ')
+    if len(price) > 1:
+        if price[1][0] == 'B': price = int(price[0])
+        elif price[1][0] == 'N': price = round(int(price[0])*1.23)
+        else: price = 0
+    else: price = 0
+    return price
+
 def FromMarpa(agd):
     if exists('MARPA.xlsx'):
         wb = load_workbook('MARPA.xlsx')
         sheet = wb.active  
-        # agd.append(name = sheet.cell(column = 1, row = 2).value, 
-        #            count = sheet.cell(column = 2, row = 2).value)
+
         obj = AGD(name = sheet.cell(column = 1, row = 2).value, 
                     count = sheet.cell(column = 2, row = 2).value,
-                    rezervacion=sheet.cell(column = 3, row = 2).value,
-                    description=sheet.cell(column = 4, row = 2).value,
-                    price=sheet.cell(column = 5, row = 2).value,
-                    country=sheet.cell(column = 6, row = 2).value)
+                    rezervacion = sheet.cell(column = 3, row = 2).value,
+                    description = sheet.cell(column = 4, row = 2).value,
+                    price = PriceToBrutto(sheet.cell(column = 5, row = 2).value),
+                    country = sheet.cell(column = 6, row = 2).value)
+
     agd.append(obj)
     return agd
